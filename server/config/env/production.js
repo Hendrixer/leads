9f1d36a7702ce6daec9c9a0396e4c339ed9dcdb0
Hendrixer <willscottmoss@gmail.com>
2016-04-28 23:06:19 -0700

    deploy

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 export default {
   logging: true,
-  port: 80,
+  port: process.env.PORT,
   appUrl: process.env.APP_URL,
   sendErrors: true,
   uploadDest: process.env.UPLOAD_DEST || path.join(process.cwd(), 'uploads'),
